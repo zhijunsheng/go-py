dfa6a5d618f64bf0d56025a152dec15aa3fa2211
Zhijun Sheng <zhijunsheng@gmail.com>
2019-12-16 06:27:03 -0500

    fixed a typo in class Move

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -3,7 +3,7 @@ from dlgo.gotypes import Player
 
 class Move():
   def __init__(self, point=None, is_pass=False, is_resign=False):
-    assert (point is not None) ^ is_pass & is_resign
+    assert (point is not None) ^ is_pass ^ is_resign
     self.point = point
     self.is_play = (self.point is not None)
     self.is_pass = is_pass
