8ae409081b39a033384261be57b1304c0faddaf0
Zhijun Sheng <zhijunsheng@gmail.com>
2019-12-15 23:23:51 -0500

    renamed game.py to bot_v_bot.py

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -1,24 +1,44 @@
-from go import agent
-from go import goboard
-from go import gotypes
-from go.utils import print_board, print_move
+from dlgo.agent import naive
+from dlgo import goboard_slow
+from dlgo import gotypes
 import time
 
-def main():
-    board_size = 9
-    game = goboard.GameState.new_game(board_size)
-    bots = {
-        gotypes.Player.black: agent.RandomBot(),
-        gotypes.Player.white: agent.RandomBot(),
-    }
-    while not game.is_over():
-        time.sleep(5.3)
+COLS = 'ABCDEFGHJKLMNOPQRST'
+STONE_TO_CHAR = {
+  None: '. ',
+  gotypes.Player.black: 'x ',
+  gotypes.Player.white: 'o ',
+}
 
-        print(chr(27) + "[2J")
-        print_board(game.board)
-        bot_move = bots[game.next_player].select_move(game)
-        print_move(game.next_player, bot_move)
-        game = game.apply_move(game.next_player, bot_move)
+def print_board(board):
+  for row in range(board.num_rows, 0, -1):
+    bump = " " if row <= 9 else ""
+    line = []
+    for col in range(1, board.num_cols + 1):
+      stone = board.get(gotypes.Point(row=row, col=col))
+      line.append(STONE_TO_CHAR[stone])
+    print('%s%d %s' % (bump, row, ''.join(line)))
+  print('   ' + ' '.join(COLS[:board.num_cols]))
+
+def main():
+  board_size = 19
+  bots = {
+    gotypes.Player.black: naive.RandomBot(),
+    gotypes.Player.white: naive.RandomBot(),
+  }
+  game = goboard_slow.GameState.new_game(board_size)
+
+  cnt = 0
+  while True:
+    player = gotypes.Player.black if cnt % 2 == 0 else gotypes.Player.white
+    cnt += 1
+    bot_move = bots[player].select_move()
+    game.board.place_stone(player, bot_move)
+    time.sleep(1.0)
+    
+    print(chr(27) + "[2J")
+    print_board(game.board)
+    print()
 
 if __name__ == '__main__':
-    main()
+  main()
