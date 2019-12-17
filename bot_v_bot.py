748419e2d898de7bdf7e54d78765ee1288ec3759
Zhijun Sheng <zhijunsheng@gmail.com>
2019-12-17 08:56:27 -0500

    added code to test capture

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -10,9 +10,6 @@ STONE_TO_CHAR = {
   gotypes.Player.white: 'o ',
 }
 
-def testCapture(game):
-  game.apply_move(goboard_slow.Move.play(gotypes.Point(5, 10))) 
-
 def print_move(player, move):
   if move.is_pass:
     move_str = 'passes'
@@ -32,6 +29,28 @@ def print_board(board):
     print('%s%d %s' % (bump, row, ''.join(line)))
   print('   ' + ' '.join(COLS[:board.num_cols]))
 
+def makeMove(game, r, c):
+  return game.apply_move(goboard_slow.Move.play(gotypes.Point(r, c))) 
+def testCapture(game):
+  print(chr(27) + "[2J")
+  game = makeMove(game, 5, 10)
+  game = makeMove(game, 6, 10) 
+  game = makeMove(game, 5, 11) 
+  game = makeMove(game, 6, 11) 
+  game = makeMove(game, 6, 9) 
+  game = makeMove(game, 1, 1)
+  game = makeMove(game, 6, 12) 
+  game = makeMove(game, 2, 1)
+  game = makeMove(game, 7, 10) 
+  game = makeMove(game, 3, 1)
+  print_board(game.board)
+  time.sleep(5.0)
+  game = makeMove(game, 7, 11) 
+  print(chr(27) + "[2J")
+  print_board(game.board)
+  game = makeMove(game, 4, 1)
+  return game
+
 def main():
   board_size = 19
   bots = {
@@ -40,7 +59,7 @@ def main():
   }
   game = goboard_slow.GameState.new_game(board_size)
 
-#  testCapture(game)
+  game = testCapture(game)
 
   while not game.is_over():
     time.sleep(2.0)
