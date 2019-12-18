1800fc43f8fc3a3e67e4b3bd0935299c46118843
Zhijun Sheng <zhijunsheng@gmail.com>
2019-12-17 21:02:44 -0500

    added function is_move_self_capture and does_move_violate_ko in class GameState

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -114,14 +114,40 @@ class GameState():
       return False
     return self.last_move.is_pass and second_last_move.is_pass
 
+  def is_move_self_capture(self, player, move):
+    if not move.is_play:
+      return False
+    next_board = copy.deepcopy(self.board)
+    next_board.place_stone(player, move.point)
+    new_string = next_board.get_go_string(move.point)
+    return new_string.num_liberties == 0
+
+  @property
+  def situation(self):
+    return (self.next_player, self.board)
+
+  def does_move_violate_ko(self, player, move):
+    if not move.is_play:
+      return False
+    next_board = copy.deepcopy(self.board)
+    next_board.place_stone(player, move.point)
+    next_situation = (player.other, next_board)
+    past_state = self.previous_state
+    while past_state is not None:
+      if past_state.situation == next_situation:
+        return True
+      past_state = past_state.previous_state
+    return False
+
   def is_valid_move(self, move):
     if self.is_over():
       return False
     if move.is_pass or move.is_resign:
       return True
     return (
-      self.board.get(move.point) is None
-      )
+      self.board.get(move.point) is None and
+      not self.is_move_self_capture(self.next_player, move) and
+      not self.does_move_violate_ko(self.next_player, move))
 
 class GoString():
   def __init__(self, color, stones, liberties):
