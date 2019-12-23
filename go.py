a81a91da1b5e284a7efcfb18473933512763296c
Peter <petershipace@gmail.com>
2019-12-23 22:00:37 +0800

    made go board

---
+++ go.py
@@ -0,0 +1,8 @@
+h = 19
+w = 19
+for i in range(h):
+  if (19 - i) < 10:
+    print(" ", end = "")
+  print(str(19 - i) + " ", end = "")
+  print(". " * w)
+print("   A B C D E F G H J K L M N O P Q R S T")
